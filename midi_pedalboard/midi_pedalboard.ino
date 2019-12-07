18822ef24f993381959c6df921cc7504aebd0e76
raltamirano <rodrigo.altamirano@gmail.com>
2019-12-07 08:38:49 -0300

    Pedal up/pedal down signaling

--- midi_pedalboard/midi_pedalboard.ino
+++ midi_pedalboard/midi_pedalboard.ino
@@ -1,28 +1,28 @@
 #include <MIDI.h>
 
-// Pin reading when button is OPEN (not pressed)
+// Pin reading when pedal is OPEN (not pressed)
 const int OPEN = HIGH;
 
-// Pin reading when button is CLOSED (pressed)
+// Pin reading when pedal is CLOSED (pressed)
 const int CLOSED = LOW;
 
 // Number of pedals
 const int PEDALS = 5;
 
 // Pin number for each pedal button
-const int PEDAL_PINS[] = { 2, 3, 4, 5, 6 }; 
+const int PEDAL_PINS[] = { 2, 4, 7, 9, 12 }; 
 
 // The debounce time; increase if the output flickers
-const unsigned long DEBOUNCE_DELAY = 50;
+const unsigned long DEBOUNCE_DELAY = 20;
 
 // Last comand sent
 int lastCommandSent;
 
 // The current reading from the input pins
-int buttonState[PEDALS];   
+int pedalState[PEDALS];   
 
 // The previous reading from the input pins
-int lastButtonState[PEDALS];   
+int lastPedalReading[PEDALS];   
 
 // MIDI init
 MIDI_CREATE_DEFAULT_INSTANCE();
@@ -34,8 +34,8 @@ void setup()
   
    for(int i=0; i<PEDALS; i++) {
      pinMode(PEDAL_PINS[i], INPUT_PULLUP);
-     buttonState[i] = OPEN;
-     lastButtonState[i] = OPEN;
+     pedalState[i] = OPEN;
+     lastPedalReading[i] = OPEN;
    }
 
    // Prepare for MIDI I/O
@@ -47,31 +47,35 @@ void setup()
 void loop()
 {
   for(int i=0; i<PEDALS; i++) {
-    if (readPedal(i) == CLOSED) {
+    int lastPedalState = pedalState[i];
+    int currentPedalState = readPedal(i);   
+    if (currentPedalState != lastPedalState) {
       int command = 60 + i; 
-      if (command != lastCommandSent) {
-        lastCommandSent = command;
+
+      if (currentPedalState == CLOSED)
         MIDI.sendNoteOn(command, 127, 1);
-      }
+      else
+        MIDI.sendNoteOff(command, 0, 1);
+
       break;
     }
   }
 }
 
 int readPedal(int pedalIndex) {
-  int reading = digitalRead(PEDAL_PINS[pedalIndex]);
+  int reading = digitalRead(PEDAL_PINS[pedalIndex]) == OPEN ? OPEN : CLOSED;
 
-  // If the switch changed, due to noise or pressing:
-  if (reading != lastButtonState[pedalIndex]) {
+  if (reading != lastPedalReading[pedalIndex]) {
     delay(DEBOUNCE_DELAY);
-    reading = digitalRead(PEDAL_PINS[pedalIndex]);    
-    if (reading != buttonState[pedalIndex]) {
-      buttonState[pedalIndex] = reading;
+    reading = digitalRead(PEDAL_PINS[pedalIndex]) == OPEN ? OPEN : CLOSED;    
+    if (reading != pedalState[pedalIndex]) {
+      pedalState[pedalIndex] = reading;
     }
   }
 
   // save the reading
-  lastButtonState[pedalIndex] = reading;  
+  lastPedalReading[pedalIndex] = reading;  
   
-  return buttonState[pedalIndex];
+  return pedalState[pedalIndex];
 }
+ 
